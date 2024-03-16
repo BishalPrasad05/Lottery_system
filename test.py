5535ecc473e11d1331f05890852b3a0f8f2b101a
Bishal Prasad <bishalprasad251122@gmail.com>
2024-03-16 21:54:06 +0000

    17 Mar 2024, 03:24

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-if (5 is 5):
-    print("5 is equal to 5")
+# if (5 is 5):
+#     print("5 is equal to 5")
 
-a=[range(10)]
+# a = [range(10)]
 
-a=[input("Enter the value :") for i in range(10)]
-print(a)
+# a = [input("Enter the value :") for i in range(10)]
+# print(a)
 
+for i in range(100):
+    print("a", end="")
