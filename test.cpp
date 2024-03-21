f90e94ff43711464f329d398bca898c754508cf3
Bishal Prasad <bishalprasad251122@gmail.com>
2024-03-21 18:18:32 +0000

    21 Mar 2024, 23:48

--- test.cpp
+++ test.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
-void mergeSort(int arr, int start, int end)
+void printArray(int arr[], int start, int end);
+
+void mergeSort(int arr[], int start, int end)
 {
+    if (start > end)
+    {
+        return;
+    }
+    printArray(arr, start, end);
+    int mid = start + (start + end) / 2;
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
 }
 
 void printArray(int arr[], int start, int end)
@@ -17,6 +27,8 @@ void printArray(int arr[], int start, int end)
 
 int main()
 {
+    int arr[7] = {9, 5, 3, 7, 6, 1, 2};
+    mergeSort(arr, 0, 6);
 
     return 0;
 }
