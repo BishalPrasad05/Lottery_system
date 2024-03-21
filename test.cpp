e20c4d5d302c025a8483aa1f5e6ebd47a4997395
Bishal Prasad <bishalprasad251122@gmail.com>
2024-03-21 18:14:33 +0000

    21 Mar 2024, 23:44

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void mergeSort(int arr, int start, int end)
+{
+}
+
+void printArray(int arr[], int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    return 0;
+}
