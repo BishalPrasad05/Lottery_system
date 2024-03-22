c1734abb92b468a915eac44db05ba4210f8a0a33
Bishal Prasad <bishalprasad251122@gmail.com>
2024-03-22 15:07:36 +0000

    22 Mar 2024, 20:37

--- test.cpp
+++ test.cpp
@@ -16,10 +16,10 @@ void merge(int arr[], int start, int mid, int end)
 
     for (int i = 0; i < size1; i++)
     {
-        arr1[i] = arr[size1 + i-1];
+        arr1[i] = arr[size1 + i - 1];
     }
 
-    for (auto var : arr2)
+    for (auto var : arr1)
     {
         cout << var << " ";
     }
